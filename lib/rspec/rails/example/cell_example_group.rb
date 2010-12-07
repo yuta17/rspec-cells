9e05c0af339c2bf4f09e2fc137d367fcbb397aa5
Nick Sutterer <apotonick@gmail.com>
2010-12-07 23:04:41 +0100

    docs updated.

--- lib/rspec/rails/example/cell_example_group.rb
+++ lib/rspec/rails/example/cell_example_group.rb
@@ -1,76 +1,8 @@
 module RSpec::Rails
-  # Extends ActionController::TestCase::Behavior to work with RSpec.
-  #
-  # == Examples
-  #
-  # == with stubs
-  #
-  #   describe WidgetsController do
-  #     describe "GET index" do
-  #       it "assigns all widgets to @widgets" do
-  #         widget = stub_model(Widget)
-  #         Widget.stub(:all) { widget }
-  #         get :index
-  #         assigns(:widgets).should eq([widget])
-  #       end
-  #     end
-  #   end
-  #
-  # === with a factory
-  #
-  #   describe WidgetsController do
-  #     describe "GET index" do
-  #       it "assigns all widgets to @widgets" do
-  #         widget = Factory(:widget)
-  #         get :index
-  #         assigns(:widgets).should eq([widget])
-  #       end
-  #     end
-  #   end
-  #
-  # === with fixtures
-  #
-  #   describe WidgetsController do
-  #     describe "GET index" do
-  #       fixtures :widgets
-  #
-  #       it "assigns all widgets to @widgets" do
-  #         get :index
-  #         assigns(:widgets).should eq(Widget.all)
-  #       end
-  #     end
-  #   end
-  #
-  # == Matchers
-  #
-  # In addition to the stock matchers from rspec-expectations, controller
-  # specs add these matchers, which delegate to rails' assertions:
-  #
-  #   response.should render_template(*args)
-  #   => delegates to assert_template(*args)
-  #
-  #   response.should redirect_to(destination)
-  #   => delegates to assert_redirected_to(destination)
-  #
-  # == Isolation from views
-  #
-  # RSpec's preferred approach to spec'ing controller behaviour is to isolate
-  # the controller from its collaborators.  By default, therefore, controller
-  # example groups do not render views. This means that a view template need
-  # not even exist in order to run a controller spec, and you can still specify
-  # which template the controller should render.
-  #
-  # == View rendering
-  #
-  # If you prefer a more integrated approach, similar to that of
-  # Rails' functional tests, you can tell controller groups to
-  # render views with the +render_views+ declaration:
-  #
-  #   describe WidgetsController do
-  #     render_views
-  #     ...
-  #
+  # Lets you call #render_cell in Rspec2. Move your cell specs to <tt>spec/cells/</tt>.
   module CellExampleGroup
+    VERSION = "0.0.1"
+    
     extend ActiveSupport::Concern
     extend RSpec::Rails::ModuleInclusion
 
