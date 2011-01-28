ee0b64e3638d008ffa3bb104de3f5d11d796f269
pboling <peter.boling@peterboling.com>
2011-01-28 18:16:21 -0500

    Railtie is not loaded in test environment so must load files directly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,10 @@ Bundler.setup
 #$:.unshift File.dirname(__FILE__) # add current dir to LOAD_PATHS
 require "rails/all"
 require 'rspec-cells'
+require 'rspec/rails'
+require 'cell/test_case'
+require 'rspec/rails/example/cell_example_group'
+
 
 module RSpecCells
   class Application < ::Rails::Application
