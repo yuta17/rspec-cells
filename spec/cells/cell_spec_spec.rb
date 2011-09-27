d376c114055ff300d6c772ed9c0cddfd98729fa8
Christian Höltje <docwhat@gerf.org>
2011-09-27 16:11:49 -0400

    Fixed responds to #render_cell test

--- spec/cells/cell_spec_spec.rb
+++ spec/cells/cell_spec_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 module RSpec::Rails
-      
+
   describe CellExampleGroup do
     let(:group) do
       RSpec::Core::ExampleGroup.describe do
         include CellExampleGroup
       end
     end
-    
+
     it "adds :type => :cell to the metadata" do
       group.metadata[:type].should eq(:cell)
     end
-    
+
     it "responds to #render_cell" do
-      group.new.should respond_to(:render_cell)
+      # Why is this weird? See https://github.com/rspec/rspec-core/issues/460
+      group.new.__should_for_example_group__ respond_to(:render_cell)
     end
   end
 end
