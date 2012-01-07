5daf68b58edb6de7955c42ed4e55f56430395cf6
Jorge Calás Lozano <calas@qvitta.net>
2012-01-07 01:00:24 +0100

    Cleanup generated files on generator tests

--- spec/cells/cell_generator_spec.rb
+++ spec/cells/cell_generator_spec.rb
@@ -6,12 +6,19 @@ describe Rspec::Generators::CellGenerator do
 
   attr_accessor :test_case, :test
 
+  DESTINATION_ROOT = File.expand_path("../../tmp", __FILE__)
+
   before(:all) do
     test_case = Class.new(Rails::Generators::TestCase)
-    test_case.destination_root = File.expand_path("../../tmp", __FILE__)
+    test_case.destination_root = DESTINATION_ROOT
     test_case.generator_class = Rspec::Generators::CellGenerator
     self.test = test_case.new :wow
     test.run_generator %w(Twitter display form)
+  end
+
+  # Cleanup after we are done testing
+  after(:all) do
+    FileUtils.rm_rf(DESTINATION_ROOT)
   end
 
   def t(line_code)
@@ -24,7 +31,7 @@ describe Rspec::Generators::CellGenerator do
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('context "cell rendering" do')
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('end')
   end
-  
+
   it 'creates display state' do
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('context "rendering display" do')
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('subject { render_cell(:twitter, :display) }')
@@ -32,7 +39,7 @@ describe Rspec::Generators::CellGenerator do
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('it { should have_selector("p", :content => "Find me in app/cells/twitter/display.html") }')
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('end')
   end
-  
+
   it 'creates form state' do
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('context "rendering form" do')
     test.assert_file "spec/cells/twitter_cell_spec.rb", t('subject { render_cell(:twitter, :form) }')
