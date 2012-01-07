8290e6eb81d6c35875a0b22fcd75687b63b2d17a
Jorge Calás Lozano <calas@qvitta.net>
2012-01-07 01:00:55 +0100

    little spec_helper cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-require 'rubygems'
-
-# wycats says...
-require 'bundler'
-Bundler.setup
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
 
 #$:.unshift File.dirname(__FILE__) # add current dir to LOAD_PATHS
 require "rails/all"
@@ -10,7 +7,6 @@ require 'rspec-cells'
 require 'rspec/rails'
 require 'cell/test_case'
 require 'rspec/rails/example/cell_example_group'
-
 
 module RSpecCells
   class Application < ::Rails::Application
