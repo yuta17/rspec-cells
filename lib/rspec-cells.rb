e57e4ce10af5d81c9c3898eeba3e440f6b83eba4
Abdelkader Boudih <terminale@gmail.com>
2014-06-11 08:56:35 +0000

    Added cell caching helper,  props to @PikachuEXE . Closes #37

--- lib/rspec-cells.rb
+++ lib/rspec-cells.rb
@@ -10,5 +10,27 @@ module RSpec
         require 'rspec/cells'
       end
     end
+
+    module Caching
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+        def enable_cell_caching!
+          before :each do
+            ActionController::Base.perform_caching = true
+          end
+          after :each do
+            ActionController::Base.perform_caching = false
+          end
+        end
+
+        def disable_cell_caching!
+          before :each do
+            ActionController::Base.perform_caching = false
+          end
+        end
+      end
+    end
+
   end
 end
