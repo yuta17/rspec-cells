fe62d7bf6d8530a957c07e139f7a695e46e75b51
Nick Sutterer <apotonick@gmail.com>
2014-08-14 09:49:33 +1000

    add a fake rails app so we can properly test capybara.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,10 @@
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 
-#$:.unshift File.dirname(__FILE__) # add current dir to LOAD_PATHS
-require 'rails'
-require 'action_controller/railtie'
-require 'action_view/railtie'
+require 'fake_app/rails_app'
+
 require 'rspec-cells'
 require 'rspec/rails'
 require 'cell/base'
 require 'rspec/cells'
 
-module RSpecCells
-  class Application < ::Rails::Application
-  end
-end
