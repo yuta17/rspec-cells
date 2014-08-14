d3a246ae2d5e11adf974c43dcaf14ba25c3a8449
Nick Sutterer <apotonick@gmail.com>
2014-08-14 10:04:51 +1000

    Revert "add a fake rails app so we can properly test capybara."
    This reverts commit fe62d7bf6d8530a957c07e139f7a695e46e75b51.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,16 @@
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 
-require 'fake_app/rails_app'
-
+#$:.unshift File.dirname(__FILE__) # add current dir to LOAD_PATHS
+require 'rails'
+require 'action_controller/railtie'
+require 'action_view/railtie'
 require 'rspec-cells'
 require 'rspec/rails'
 require 'cell/base'
 require 'rspec/cells'
 
+module RSpecCells
+  class Application < ::Rails::Application
+  end
+end
